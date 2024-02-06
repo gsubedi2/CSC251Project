373b9740dd3f72551140672fc2307897fce80226
Gaurab Subedi <gsubedi2@email.cpcc.edu>
2024-02-05 22:12:56 -0500

    Project Part 2
    Added documentation comments

--- Policy.java
+++ Policy.java
@@ -1,15 +1,16 @@
 public class Policy
 {
-   private int policyNumber;
-   private String providerName;
-   private String policyholderFirstName;
-   private String policyholderLastName;
-   private int policyholderAge;
-   private String smokingStatus;
-   private double height;
-   private double weight;
+    // Instance variables
+    private int policyNumber;
+    private String providerName;
+    private String policyholderFirstName;
+    private String policyholderLastName;
+    private int policyholderAge;
+    private String smokingStatus;
+    private double height;
+    private double weight;
    
-   // No-arg constructor
+   //no-arg constructor
    public Policy()
    {
       policyNumber = 0;
@@ -22,7 +23,17 @@ public class Policy
       weight = 0.0;
    }
    
-   //Constructor with arguements
+   /**
+      Constructor that accepts an arguement for the Policy.
+      @param pNumber the policy number
+      @param pName the provider Name
+      @param firstName the policy holder first Name
+      @param lastName the policy holder last name
+      @param age the policy holder age
+      @param sStatus the smoking status
+      @param ht the height
+      @param wt the weight
+   */
    public Policy(int pNumber,String pName, String firstName,String lastName, int age, String sStatus, double ht, double wt)
    {
       policyNumber = pNumber;
@@ -34,99 +45,147 @@ public class Policy
       height = ht;
       weight = wt;
    }
-   //setters and getters for PolicyNumber
+   /**
+      Mutator (setter) method
+      @Param pNumber the policy number
+   */
    public void setPolicyNumber(int pNumber)
    {
       policyNumber = pNumber;
    }
-   
+   /**
+      Accessor (getter) method
+      @return the policy number
+   */
    public int getPolicyNumber()
    {
       return policyNumber;
-   }
-   
-   //Settera and getters for Provider Name
+   } 
+   /**
+      Mutator (setter) method
+      @Param pname the provider name
+   */
    public void setProviderName(String pName)
    {
       providerName = pName;
    }
-   
+   /**
+      Accessor (getter) method
+      @return the provider name
+   */
    public String getProviderName()
    {
       return providerName;
    }
-   
-   //Setters and getters for Policy Holder First name
+   /**
+      Mutator (setter) method
+      @Param firstName the policy holder first name
+   */  
    public void setPolicyHolderFirstName(String firstName)
    {
       policyholderFirstName = firstName;
    }
-   
+   /**
+      Accessor (getter) method
+      @return the policy holder first name
+   */
    public String getPolicyHolderFirstName()
    {
       return policyholderFirstName;
    }
-   
-   //setters and getters for policy holder last name
+   /**
+      Mutator (setter) method
+      @Param firstName the policy holder last name
+   */ 
    public void setPolicyHolderLastName(String lastName)
    {
       policyholderLastName = lastName;
    }
-   
+   /**
+      Accessor (getter) method
+      @return the policy holder last name
+   */
    public String getPolicyHolderLastName()
    {
       return policyholderLastName;
    }
-   
-   //setters and getters for policy holder age
+    /**
+      Mutator (setter) method
+      @Param age the policy holder age
+   */ 
    public void setPolicyHolderAge(int age)
    {
       policyholderAge = age;
    }
-   
+   /**
+      Accessor (getter) method
+      @return the policy age
+   */
    public int getPolicyHolderAge()
    {
       return policyholderAge;
    }
-   
-   //setters and getters for Smoking Status
+   /**
+      Mutator (setter) method
+      @Param sStatus the policy holder smoking status
+   */
    public void setSmokingStatus(String sStatus)
    {
       smokingStatus = sStatus;
    }
+   /**
+      Accessor (getter) method
+      @return the policy holder smoking status
+   */
    public String getSmokingStatus()
    {
       return smokingStatus;
    }
-   
-   //setters and getters for height
+   /**
+      Mutator (setter) method
+      @Param ht the policy holder height
+   */
    public void setHeight(double ht)
    {
       height = ht;
    }
-   
+   /**
+      Accessor (getter) method
+      @return the policy holder height
+   */
    public double getHeight()
    {
       return height;
    }
-   
-   //setters and getters for weight
+   /**
+      Mutator (setter) method
+      @Param ht the policy holder weight
+   */
    public void setWeight(double wt)
    {
       weight = wt;
    }
-   
+   /**
+      Accessor (getter) method
+      @return the policy holder weight
+   */
    public double getWeight()
    {
       return weight;
    }
    
-   //BWI CALCULATION
+   /**
+      Method for getting the calculation for the BMI of the policyholder
+      @return the BMI of the policy holder
+   */
    public double calculateBWI()
    {
       return (weight * 703) / (height * height);
    }
-   
+   /**
+      Method for calculating and returning the price of the insurance policy
+      @return the policy price
+   */
    public double calculatePolicyPrice()
    {
       //get the bmi value
