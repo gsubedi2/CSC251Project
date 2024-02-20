27f7c86af5734b97de46b5b33383e60d9b93ff8b
Gaurab Subedi <gsubedi2@email.cpcc.edu>
2024-02-19 23:22:57 -0500

    Squashed commit of the following:
    commit d32819a3e57ee1f4090ae41c73056ec0398757a4
    Author: Gaurab Subedi <gsubedi2@email.cpcc.edu>
    Date:   Mon Feb 19 23:20:53 2024 -0500
    
        All code
    
    commit 602bc00e53cbeaa002e073d80f4f3616e1dd4f19
    Author: Gaurab Subedi <gsubedi2@email.cpcc.edu>
    Date:   Mon Feb 19 21:37:44 2024 -0500
    
        Create PolicyHolder.java

--- Policy.java
+++ Policy.java
@@ -1,214 +1,71 @@
 public class Policy
 {
-    // Instance variables
+    // Instance variable
     private int policyNumber;
     private String providerName;
-    private String policyholderFirstName;
-    private String policyholderLastName;
-    private int policyholderAge;
-    private String smokingStatus;
-    private double height;
-    private double weight;
-   
-   //no-arg constructor
-   public Policy()
+    private PolicyHolder policyHolder;
+
+    // No-arg constructor
+    public Policy()
+    {
+        // Initialize default values or leave them as-is based on your requirements
+        policyNumber = 0;
+        providerName = "";
+        policyHolder = new PolicyHolder();
+    }
+
+    // Parameterized constructor
+    public Policy(int pNumber, String pName, PolicyHolder pHolder)
+    {
+        policyNumber = pNumber;
+        providerName = pName;
+        policyHolder = new PolicyHolder(pHolder);//to avoid security holes       
+    }
+
+    // Accessor methods
+    public int getPolicyNumber()
+    {
+        return policyNumber;
+    }
+
+    public String getProviderName()
+    {
+        return providerName;
+    }
+
+    public PolicyHolder getPolicyHolder()
+    {
+        return policyHolder;
+    }
+
+    // Method for calculating policy price
+    public double calculatePolicyPrice()
+    {
+        double bmi = policyHolder.calculateBMI();
+        double additionalFee = (bmi - 35) * 20;
+        double policyPrice = 600.00;
+
+        if (policyHolder.getAge() > 50)
+        {
+            policyPrice += 75.00;
+        }
+        if (policyHolder.getSmokingStatus().equalsIgnoreCase("smoker"))
+        {
+            policyPrice += 100.00;
+        }
+        if (bmi > 35)
+        {
+            policyPrice += additionalFee;
+        }
+
+        return policyPrice;
+    }
+
+   public String toString()
    {
-      policyNumber = 0;
-      providerName = "";
-      policyholderFirstName = "";
-      policyholderLastName = "";
-      policyholderAge = 0;
-      smokingStatus = "";
-      height = 0.0;
-      weight = 0.0;
+    return "Policy Number: " + policyNumber +
+           "\nProvider Name: " + providerName +
+           "\n" + policyHolder.toString() + 
+           "\nPolicy Price: $" + calculatePolicyPrice();
    }
-   
-   /**
-      Constructor that accepts an arguement for the Policy.
-      @param pNumber the policy number
-      @param pName the provider Name
-      @param firstName the policy holder first Name
-      @param lastName the policy holder last name
-      @param age the policy holder age
-      @param sStatus the smoking status
-      @param ht the height
-      @param wt the weight
-   */
-   public Policy(int pNumber,String pName, String firstName,String lastName, int age, String sStatus, double ht, double wt)
-   {
-      policyNumber = pNumber;
-      providerName = pName;
-      policyholderFirstName = firstName;
-      policyholderLastName = lastName;
-      policyholderAge = age;
-      smokingStatus = sStatus;
-      height = ht;
-      weight = wt;
-   }
-   /**
-      Mutator (setter) method
-      @Param pNumber the policy number
-   */
-   public void setPolicyNumber(int pNumber)
-   {
-      policyNumber = pNumber;
-   }
-   /**
-      Accessor (getter) method
-      @return the policy number
-   */
-   public int getPolicyNumber()
-   {
-      return policyNumber;
-   } 
-   /**
-      Mutator (setter) method
-      @Param pname the provider name
-   */
-   public void setProviderName(String pName)
-   {
-      providerName = pName;
-   }
-   /**
-      Accessor (getter) method
-      @return the provider name
-   */
-   public String getProviderName()
-   {
-      return providerName;
-   }
-   /**
-      Mutator (setter) method
-      @Param firstName the policy holder first name
-   */  
-   public void setPolicyHolderFirstName(String firstName)
-   {
-      policyholderFirstName = firstName;
-   }
-   /**
-      Accessor (getter) method
-      @return the policy holder first name
-   */
-   public String getPolicyHolderFirstName()
-   {
-      return policyholderFirstName;
-   }
-   /**
-      Mutator (setter) method
-      @Param firstName the policy holder last name
-   */ 
-   public void setPolicyHolderLastName(String lastName)
-   {
-      policyholderLastName = lastName;
-   }
-   /**
-      Accessor (getter) method
-      @return the policy holder last name
-   */
-   public String getPolicyHolderLastName()
-   {
-      return policyholderLastName;
-   }
-    /**
-      Mutator (setter) method
-      @Param age the policy holder age
-   */ 
-   public void setPolicyHolderAge(int age)
-   {
-      policyholderAge = age;
-   }
-   /**
-      Accessor (getter) method
-      @return the policy age
-   */
-   public int getPolicyHolderAge()
-   {
-      return policyholderAge;
-   }
-   /**
-      Mutator (setter) method
-      @Param sStatus the policy holder smoking status
-   */
-   public void setSmokingStatus(String sStatus)
-   {
-      smokingStatus = sStatus;
-   }
-   /**
-      Accessor (getter) method
-      @return the policy holder smoking status
-   */
-   public String getSmokingStatus()
-   {
-      return smokingStatus;
-   }
-   /**
-      Mutator (setter) method
-      @Param ht the policy holder height
-   */
-   public void setHeight(double ht)
-   {
-      height = ht;
-   }
-   /**
-      Accessor (getter) method
-      @return the policy holder height
-   */
-   public double getHeight()
-   {
-      return height;
-   }
-   /**
-      Mutator (setter) method
-      @Param ht the policy holder weight
-   */
-   public void setWeight(double wt)
-   {
-      weight = wt;
-   }
-   /**
-      Accessor (getter) method
-      @return the policy holder weight
-   */
-   public double getWeight()
-   {
-      return weight;
-   }
-   
-   /**
-      Method for getting the calculation for the BMI of the policyholder
-      @return the BMI of the policy holder
-   */
-   public double calculateBWI()
-   {
-      return (weight * 703) / (height * height);
-   }
-   /**
-      Method for calculating and returning the price of the insurance policy
-      @return the policy price
-   */
-   public double calculatePolicyPrice()
-   {
-      //get the bmi value
-      double bmi = calculateBWI();
-      //additional fee
-      double additionalFee = (bmi - 35) * 20;
-      //base fee
-      double policyPrice = 600.00;
-      
-      if(policyholderAge > 50)
-      {
-         policyPrice += 75.00;
-      }
-      if(smokingStatus.equalsIgnoreCase("smoker"))
-      {
-         policyPrice += 100.00;
-      }
-      if(bmi > 35)
-      {
-         policyPrice += additionalFee;
-      }
-      return policyPrice;
-   }
-   
-   
 }
